f338a76b449b38966b4626c6c85d8bbf506362fb
Asif Mujtaba <asif.mujtaba@tigerit.com>
2022-10-06 00:20:39 +0600

    Add HTTP result and error type

--- HTTP-networking-stack/Networking/HTTPResponse.swift
+++ HTTP-networking-stack/Networking/HTTPResponse.swift
@@ -26,3 +26,40 @@ public struct HTTPResponse {
     
     public var headers: [AnyHashable: Any] { response.allHeaderFields }
 }
+
+public typealias HTTPResult = Result<HTTPResponse, HTTPError>
+
+public struct HTTPError: Error {
+    public let code: Code
+    
+    public let request: HTTPRequest
+    
+    public let response: HTTPResponse?
+    
+    public let underlyingError: Error?
+    
+    public enum Code {
+        case invalidRequest
+        case cannotConnect
+        case cancelled
+        case insecureConnection
+        case invalidResponse
+        case unknown
+    }
+}
+
+extension HTTPResult {
+    public var request: HTTPRequest {
+        switch self {
+        case .success(let response): return response.request
+        case .failure(let error): return error.request
+        }
+    }
+    
+    public var response: HTTPResponse? {
+        switch self {
+        case .success(let response): return response
+        case .failure(let error): return error.response
+        }
+    }
+}
